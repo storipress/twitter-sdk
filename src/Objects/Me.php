112787d7ffe46d67f04ee3a71c0de84285271c0f
bepsvpt <8221099+bepsvpt@users.noreply.github.com>
2024-01-30 17:12:22 +0800

    feat: add optional fields type hint to me object

--- src/Objects/Me.php
+++ src/Objects/Me.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Storipress\Twitter\Objects;
 
+use stdClass;
+
 class Me extends TwitterObject
 {
     public string $id;
@@ -11,4 +13,33 @@ class Me extends TwitterObject
     public string $name;
 
     public string $username;
+
+    public ?string $created_at = null;
+
+    public ?string $most_recent_tweet_id = null;
+
+    public ?bool $protected = null;
+
+    public ?stdClass $withheld = null;
+
+    public ?string $location = null;
+
+    public ?string $url = null;
+
+    public ?string $description = null;
+
+    public ?bool $verified = null;
+
+    /**
+     * @var 'blue'|'business'|'government'|'none'|null
+     */
+    public ?string $verified_type = null;
+
+    public ?stdClass $entities = null;
+
+    public ?string $profile_image_url = null;
+
+    public ?stdClass $public_metrics = null;
+
+    public ?string $pinned_tweet_id = null;
 }
